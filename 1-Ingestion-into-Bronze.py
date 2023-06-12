38f5b71d723d82339e57abb4f84bc6c455d3798d
sachinpatilb <sachinpatilb>
2023-06-12 16:44:16 +0000

    Parameterised site

--- 1-Ingestion-into-Bronze.py
+++ 1-Ingestion-into-Bronze.py
@@ -9,21 +9,58 @@
 
 # COMMAND ----------
 
+# Define widget
+dbutils.widgets.dropdown("site", "primary", ["primary","secondary"],"Choose Primary or Secondary site")
+
+# COMMAND ----------
+
+# Get the value of site
+site=dbutils.widgets.get("site")
+
+# COMMAND ----------
+
+if site =="primary":
+  src_path=primary_config['source_path']
+  checkpoint_path=primary_config['checkpoint_path']
+  schema_path=primary_config['schema_path']
+  raw_path=primary_config['raw_path']
+  db=primary_config['database']
+  bronze_table=primary_config['bronze_table']
+  silver_table=primary_config['silver_table']
+  gold_table_a=primary_config['gold_table_a']
+  gold_table_b=primary_config['gold_table_b']
+  bronze_stream=primary_config['bronze_stream']
+  silver_stream=primary_config['silver_stream']
+  gold_stream_a=primary_config['gold_stream_a']
+  gold_stream_b=primary_config['gold_stream_b']
+else:
+  src_path=secondary_config['source_path']
+  checkpoint_path=secondary_config['checkpoint_path']
+  schema_path=secondary_config['schema_path']
+  raw_path=secondary_config['raw_path']
+  db=secondary_config['database']
+  bronze_table=secondary_config['bronze_table']
+  silver_table=secondary_config['silver_table']
+  gold_table_a=secondary_config['gold_table_a']
+  gold_table_b=secondary_config['gold_table_b']
+  bronze_stream=primary_config['bronze_stream']
+  silver_stream=primary_config['silver_stream']
+  gold_stream_a=primary_config['gold_stream_a']
+  gold_stream_b=primary_config['gold_stream_b']
+
+# COMMAND ----------
+
 # Read the input CSV file using AutoLoader
 _=(spark.readStream.format("cloudFiles")
             .option("cloudFiles.format","csv")
             .option("cloudFiles.inferSchema", "true")
-            .option("cloudFiles.schemaLocation", primary_config['schema_path'])
+            .option("cloudFiles.schemaLocation", schema_path)
             .option("cloudFiles.maxFilesPerTrigger", "1")
             .option("header",True)
-            .load(primary_config['source_path'])
+            .load(src_path)
             .writeStream
             .format("delta")
-            .option("checkpointLocation",primary_config['checkpoint_path']+"/bronze")
-            .queryName(primary_config['bronze_stream'])
-            .table("bronze_txn")
+            .option("checkpointLocation",checkpoint_path+"/bronze")
+            .queryName(bronze_stream)
+            .table(bronze_table)
 )
-
-# COMMAND ----------
-
-
