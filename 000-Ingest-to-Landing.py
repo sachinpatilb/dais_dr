3b1232d4f99d105dadf2086447119b6511ea69cf
shasidhar.eranti@databricks.com <shasidhar.eranti@databricks.com>
2023-06-26 22:22:51 +0000

    logging file

--- 000-Ingest-to-Landing.py
+++ 000-Ingest-to-Landing.py
@@ -35,6 +35,7 @@ if(site == 'primary' or site == 'primary2') :
     raw_path = file.path
     src_path = os.path.join(src_dir, file.name)
     dbutils.fs.cp(raw_path, src_path)
+    print(f"Copying {raw_path} to {src_path}")
     time.sleep(1) # sleep for 1 second between copy operations
 else:
   # set current datebase context
