7382fd39f1e9faa2a47087cc5944c44acd7de56c
sachinpatilb <sachinpatilb>
2023-06-27 04:12:08 +0000

    Added jobs for nightly job

---
+++ 2-Bronze-to-Silver-nightly.py
@@ -0,0 +1,69 @@
+# Databricks notebook source
+# MAGIC %md ##Introduction
+# MAGIC
+# MAGIC In this notebook, we will read data from the bronze table in real time and carry out stateless transactions
+# MAGIC - Data qualilty checks
+# MAGIC - Data type change for event time
+# MAGIC - Adding new features required for fraud detection
+
+# COMMAND ----------
+
+# MAGIC %run "./0-config"
+
+# COMMAND ----------
+
+# Define widget
+dbutils.widgets.dropdown("site", "primary", ["primary","secondary","primary2","secondary2"],"Choose Primary or Secondary site")
+
+# COMMAND ----------
+
+# Get the value of site
+site=dbutils.widgets.get("site")
+
+# COMMAND ----------
+
+config = get_configs(site,{})
+
+# COMMAND ----------
+
+# set current datebase context
+_ = spark.catalog.setCurrentDatabase(config['db'])
+
+# COMMAND ----------
+
+from pyspark.sql.functions import *
+
+if(site == 'primary' or site == 'primary2' or site == 'secondary2') :
+  _ = spark \
+    .readStream \
+    .table(config['bronze_table']) \
+    .withColumn("event_time",to_timestamp("event_time", "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")) \
+    .withColumn("diffOrig", col("newBalanceOrig")-col("oldBalanceOrig")) \
+    .withColumn("diffDest", col("newBalanceDest")-col("oldBalanceDest")) \
+    .filter(col("customer_id").isNotNull()) \
+    .filter(col("id").isNotNull()) \
+    .writeStream \
+    .format("delta") \
+    .outputMode("append") \
+    .option("checkpointLocation",config['checkpoint_path']+"/silver") \
+    .queryName(config['silver_stream'])\
+    .table(config['silver_table'])
+else :
+  starting_offset = spark.read.table("offset_tracker").agg(max(col("secondary_bronze_version"))).collect()[0][0]
+  print(starting_offset)
+  _ = spark \
+    .readStream \
+    .option("startingVersion", starting_offset) \
+    .table(config['bronze_table']) \
+    .withColumn("event_time",to_timestamp("event_time", "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")) \
+    .withColumn("diffOrig", col("newBalanceOrig")-col("oldBalanceOrig")) \
+    .withColumn("diffDest", col("newBalanceDest")-col("oldBalanceDest")) \
+    .filter(col("customer_id").isNotNull()) \
+    .filter(col("id").isNotNull()) \
+    .writeStream \
+    .format("delta") \
+    .trigger(Trigger.AvailableNow) \
+    .outputMode("append") \
+    .option("checkpointLocation",config['checkpoint_path']+"/silver") \
+    .queryName(config['silver_stream'])\
+    .table(config['silver_table'])
---
+++ 3b-Silver-to-Gold-nightly.py
@@ -0,0 +1,47 @@
+# Databricks notebook source
+# MAGIC %md ##Introduction
+# MAGIC
+# MAGIC Fraud detection use case needs number of transactions carried out by a customer in last one hour. If this jumps over the predefined threshold, it is flagged as a possible fraud that might need further investigation.
+# MAGIC
+# MAGIC In this notebook, we are reading the cleansed transactions from silver table and we are using stateful transformation with window and watermark to write the transaction in gold table
+
+# COMMAND ----------
+
+# MAGIC %run "./0-config"
+
+# COMMAND ----------
+
+# Define widget
+dbutils.widgets.dropdown("site", "primary", ["primary","secondary","primary2","secondary2"],"Choose Primary or Secondary site")
+
+# COMMAND ----------
+
+# Get the value of site
+site=dbutils.widgets.get("site")
+
+# COMMAND ----------
+
+config = get_configs(site,{})
+
+# COMMAND ----------
+
+# set current datebase context
+_ = spark.catalog.setCurrentDatabase(config['db'])
+
+# COMMAND ----------
+
+from pyspark.sql.functions import *
+
+gold_txn_df = spark.readStream \
+  .table(config['silver_table']) \
+  .withWatermark("event_time", "60 minutes") \
+  .groupBy(window("event_time", "60 minutes"), "customer_id") \
+  .agg(expr("count(customer_id) as no_of_txn")) \
+  .selectExpr("window.start as start_time", "customer_id", "no_of_txn") \
+  .writeStream \
+  .format("delta") \
+  .trigger(Trigger.AvailableNow) \
+  .queryName(config['gold_stream'])\
+  .outputMode("append") \
+  .option("checkpointLocation",config['checkpoint_path']+"/gold") \
+  .table(config['gold_table'])
